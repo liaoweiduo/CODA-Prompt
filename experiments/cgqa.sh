90928e6ed920b45fe9cca560885f61e1a6f82cf2
weiduliao2 <liaowd@mail.sustech.edu.cn>
2024-02-20 22:28:09 +0800

    run pmo-try

--- experiments/cgqa.sh
+++ experiments/cgqa.sh
@@ -29,10 +29,8 @@ LOGNAME=pmo-try
 python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
     --learner_type prompt --learner_name ${LEARNERNAME} \
     --prompt_param 100 8 0.0 2000 \
-    --log_dir ${OUTDIR}/${LOGNAME}# \
-#    --debug_mode 1
+    --log_dir ${OUTDIR}/${LOGNAME}
 date
-
 
 # CODA-P-Replay
 #
