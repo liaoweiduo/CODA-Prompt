76b2de02201855b4c76709f43d5663f271efd683
weiduliao2 <liaowd@mail.sustech.edu.cn>
2024-04-01 20:30:20 +0800

    pmo-full-min-use-puniform-20-3-c1

--- experiments/cgqa.sh
+++ experiments/cgqa.sh
@@ -28,10 +28,10 @@ mkdir -p $OUTDIR
 #    arg 6 = mask_mode: 0: maskout or 1: use
 LEARNERTYPE=pmo
 LEARNERNAME=PMOPrompt
-LOGNAME=pmo-full-min-use-p0-20-3-c1
+LOGNAME=pmo-full-min-use-puniform-20-3-c1
 python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
     --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
-    --prompt_param 100 8 0.0 3 0.0 1 \
+    --prompt_param 100 8 0.0 3 -10001 1 \
     --log_dir ${OUTDIR}/${LOGNAME}
 date
 
