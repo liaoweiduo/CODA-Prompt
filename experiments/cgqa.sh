c4e045d2388b633b0ba9371740a459b541395ab2
weiduliao2 <liaowd@mail.sustech.edu.cn>
2024-05-24 13:14:01 +0800

    coda-cond-ipmd-FPS20-lr1e-4

--- experiments/cgqa.sh
+++ experiments/cgqa.sh
@@ -7,7 +7,7 @@ N_CLASS=100
 OUTDIR=outputs/${DATASET}/10-task
 
 # hard coded inputs
-GPUID='0 1'   # '0 1 2 3'
+GPUID='0'   # '0 1 2 3'
 CONFIG=configs/cgqa_prompt.yaml
 REPEAT=1
 OVERWRITE=0
@@ -27,14 +27,14 @@ mkdir -p $OUTDIR
 #    arg 5 = mask: 0.0; -10000: randn; -10001: uniform; -10002: ortho; -10003: None
 #    arg 6 = mask_mode: 0: maskout or 1: use
 #    arg 7 = hv coeff, -1 to use LCQP
-LEARNERTYPE=pmo
-LEARNERNAME=PMOPrompt
-LOGNAME=pmo-condip-full-min-use-pNone-bs32-e50-e2-lr1e-3-20-2-sumhv-norm-anchor-cossim-LCQP_kap-FPS20
-python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-    --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
-    --prompt_param 20 2 0.0 2 -10003 1 -1 \
-    --log_dir ${OUTDIR}/${LOGNAME}
-date
+#LEARNERTYPE=pmo
+#LEARNERNAME=PMOPrompt
+#LOGNAME=pmo-condip-full-min-use-pNone-bs32-e50-e2-lr1e-3-20-2-sumhv-norm-anchor-cossim-LCQP_kap-FPS20
+#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#    --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
+#    --prompt_param 20 2 0.0 2 -10003 1 -1 \
+#    --log_dir ${OUTDIR}/${LOGNAME}
+#date
 
 # CODA-P-Replay
 #
@@ -81,10 +81,10 @@ date
 #    arg 1 = prompt component pool size     20 for fixed prompt size
 #    arg 2 = prompt length
 #    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name CODAPromptCond \
-#    --prompt_param 20 2 0.0 \
-#    --log_dir ${OUTDIR}/coda-cond-ipmd-FPS20
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name CODAPromptCond \
+    --prompt_param 20 2 0.0 \
+    --log_dir ${OUTDIR}/coda-cond-ipmd-FPS20-lr1e-4
 
 # DualPrompt
 #
