e54c152f4bf802f627edacc1e1498d9f10e9c819
weiduliao2 <liaowd@mail.sustech.edu.cn>
2024-05-30 19:51:09 +0800

    explicitly use concept label

--- experiments/cgqa.sh
+++ experiments/cgqa.sh
@@ -84,9 +84,9 @@ mkdir -p $OUTDIR
 python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
     --learner_type prompt --learner_name CODAPromptCond \
     --prompt_param 21 2 0.0 \
-    --oracle_flag --upper_bound_flag \
-    --log_dir ${OUTDIR}/coda-cond-ip-FPS21-normalattn-mtl
-# -mtl -cheating-conceptasaqk
+    --log_dir ${OUTDIR}/coda-cond-ip-FPS21-normalattn-cheating-conceptasaqk-epoch5
+#    --oracle_flag --upper_bound_flag \
+# -mtl
 
 # DualPrompt
 #
--- trainer.py
+++ trainer.py
@@ -98,7 +98,8 @@ class Trainer:
             resize_imnet = True
         else:
             resize_imnet = False
-        return_concepts = False
+
+        return_concepts = True
         train_transform = dataloaders.utils.get_transform(dataset=args.dataset, phase='train', aug=args.train_aug, resize_imnet=resize_imnet)
         test_transform  = dataloaders.utils.get_transform(dataset=args.dataset, phase='test', aug=args.train_aug, resize_imnet=resize_imnet)
         self.train_dataset = Dataset(args.dataroot, train=True, lab = args.oracle_flag, tasks=self.tasks,
