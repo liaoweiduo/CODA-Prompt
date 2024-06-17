0d6fda40eb500f03d8b45d16d03c35a1163fdf72
weiduliao2 <liaowd@mail.sustech.edu.cn>
2024-06-17 11:15:05 +0800

    coda-epoch30-first30-1

--- experiments/cgqa.sh
+++ experiments/cgqa.sh
@@ -7,7 +7,7 @@ N_CLASS=100
 OUTDIR=outputs/${DATASET}/10-task
 
 # hard coded inputs
-GPUID='0 1 2 3'   # '0 1 2 3'
+GPUID='0'   # '0 1 2 3'
 CONFIG=configs/cgqa_prompt.yaml
 REPEAT=1
 OVERWRITE=0
@@ -28,15 +28,15 @@ mkdir -p $OUTDIR
 #    arg 6 = mask_mode: 0: maskout or 1: use
 #    arg 7 = hv coeff, -1 to use LCQP
 #    --oracle_flag --upper_bound_flag \
-LEARNERTYPE=pmo
-LEARNERNAME=PMOPrompt
-LOGNAME=pmo-f4m-epoch30-mtl
-python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-    --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
-    --prompt_param 21 2 0.0 2 -10003 1 -1 \
-    --oracle_flag --upper_bound_flag \
-    --log_dir ${OUTDIR}/${LOGNAME}
-date
+#LEARNERTYPE=pmo
+#LEARNERNAME=PMOPrompt
+#LOGNAME=pmo-f4m-epoch30-mtl
+#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#    --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
+#    --prompt_param 21 2 0.0 2 -10003 1 -1 \
+#    --oracle_flag --upper_bound_flag \
+#    --log_dir ${OUTDIR}/${LOGNAME}
+#date
 
 # CODA-P-Replay
 #
@@ -72,10 +72,10 @@ date
 #    arg 1 = prompt component pool size     20 for fixed prompt size
 #    arg 2 = prompt length
 #    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name CODAPrompt \
-#    --prompt_param 100 8 0.0 \
-#    --log_dir ${OUTDIR}/coda-epoch30-first30
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name CODAPrompt \
+    --prompt_param 100 8 0.0 \
+    --log_dir ${OUTDIR}/coda-epoch30-first30-1
 
 # PATCH-P
 #
