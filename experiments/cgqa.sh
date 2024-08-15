70c140fc975aaf7f0f176b5f835aad5dee02b47f
pomelo <liaowd@mail.sustech.edu.cn>
2024-08-15 21:46:43 +0800

    slot-k3-recon-lr3e-4

--- experiments/cgqa.sh
+++ experiments/cgqa.sh
@@ -28,10 +28,10 @@ mkdir -p $OUTDIR
 #    --debug_mode 1 \
 LEARNERTYPE=slotmo
 LEARNERNAME=SLOTPrompt
-LOGNAME=slot-recon-lr3e-4
+LOGNAME=slot-k3-recon-lr3e-4
 python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
     --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
-    --prompt_param 100 8 30 5 \
+    --prompt_param 100 8 3 2 \
     --log_dir ${OUTDIR}/${LOGNAME}
 date
 
