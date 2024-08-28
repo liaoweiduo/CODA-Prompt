2291083190c7e2baf4155950f93fdb4d69da0f5d
pomelo <liaowd@mail.sustech.edu.cn>
2024-08-28 14:10:27 +0800

    slot-k5-recon-klresponse-lr1e-4

--- learners/slotmo.py
+++ learners/slotmo.py
@@ -580,7 +580,7 @@ class SLOTPrompt(Prompt):
             self.epoch_log['scaler']['Idx'].append(self.epoch)
             self.epoch_log['scaler']['Value'].append(s2p_loss.item())
 
-            coeff = 0.01
+            coeff = 0.0
             total_loss = loss + coeff * s2p_loss
             total_loss.backward()
 
