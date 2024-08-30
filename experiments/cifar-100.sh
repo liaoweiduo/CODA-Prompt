f0f6fa24d89ea1fe159571098baae553b15f19d8
pomelo <liaowd@mail.sustech.edu.cn>
2024-08-30 22:25:22 +0800

    cifar100

--- experiments/cifar-100.sh
+++ experiments/cifar-100.sh
@@ -29,60 +29,60 @@ mkdir -p $OUTDIR
 #    arg 5 = p
 #    --oracle_flag --upper_bound_flag \
 #    --debug_mode 1 \
-LEARNERTYPE=slotmo
-LEARNERNAME=SLOTPrompt
-LOGNAME=slot-k5-recon-noreg-lr1e-4
-python -u run.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-    --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
-    --prompt_param 100 8 5 0.0 30 \
-    --log_dir ${OUTDIR}/${LOGNAME}
-date
+#LEARNERTYPE=slotmo
+#LEARNERNAME=SLOTPrompt
+#LOGNAME=slot-k5-recon-noreg-lr1e-4
+#python -u run.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#    --learner_type ${LEARNERTYPE} --learner_name ${LEARNERNAME} \
+#    --prompt_param 100 8 5 0.0 30 \
+#    --log_dir ${OUTDIR}/${LOGNAME}
+#date
 
-## CODA-P
-##
-## prompt parameter args:
-##    arg 1 = prompt component pool size
-##    arg 2 = prompt length
-##    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name CODAPrompt \
-#    --prompt_param 100 8 0.0 \
-#    --log_dir ${OUTDIR}/coda-p
+# CODA-P
 #
-## DualPrompt
-##
-## prompt parameter args:
-##    arg 1 = e-prompt pool size (# tasks)
-##    arg 2 = e-prompt pool length
-##    arg 3 = g-prompt pool length
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name DualPrompt \
-#    --prompt_param 10 20 6 \
-#    --log_dir ${OUTDIR}/dual-prompt
+# prompt parameter args:
+#    arg 1 = prompt component pool size
+#    arg 2 = prompt length
+#    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name CODAPrompt \
+    --prompt_param 100 8 0.0 \
+    --log_dir ${OUTDIR}/coda-p
+
+# DualPrompt
 #
-## L2P++
-##
-## prompt parameter args:
-##    arg 1 = e-prompt pool size (# tasks)
-##    arg 2 = e-prompt pool length
-##    arg 3 = -1 -> shallow, 1 -> deep
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name L2P \
-#    --prompt_param 30 20 -1 \
-#    --log_dir ${OUTDIR}/l2p++
+# prompt parameter args:
+#    arg 1 = e-prompt pool size (# tasks)
+#    arg 2 = e-prompt pool length
+#    arg 3 = g-prompt pool length
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name DualPrompt \
+    --prompt_param 10 20 6 \
+    --log_dir ${OUTDIR}/dual-prompt
+
+# L2P++
 #
-## FT
-#python -u run.py --config $CONFIG_FT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type default --learner_name FinetunePlus \
-#    --log_dir ${OUTDIR}/ft++
-#
-## FT++
-#python -u run.py --config $CONFIG_FT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type default --learner_name NormalNN \
-#    --log_dir ${OUTDIR}/ft
-#
-## Offline
-#python -u run.py --config $CONFIG_FT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type default --learner_name NormalNN --upper_bound_flag \
-#    --log_dir ${OUTDIR}/offline
+# prompt parameter args:
+#    arg 1 = e-prompt pool size (# tasks)
+#    arg 2 = e-prompt pool length
+#    arg 3 = -1 -> shallow, 1 -> deep
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name L2P \
+    --prompt_param 30 20 -1 \
+    --log_dir ${OUTDIR}/l2p++
+
+# FT
+python -u run.py --config $CONFIG_FT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type default --learner_name FinetunePlus \
+    --log_dir ${OUTDIR}/ft++
+
+# FT++
+python -u run.py --config $CONFIG_FT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type default --learner_name NormalNN \
+    --log_dir ${OUTDIR}/ft
+
+# Offline
+python -u run.py --config $CONFIG_FT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type default --learner_name NormalNN --upper_bound_flag \
+    --log_dir ${OUTDIR}/offline
 
