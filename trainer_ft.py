00d883abc5928697dc66bc65924445ec7792d6f7
pomelo <liaowd@mail.sustech.edu.cn>
2024-09-09 23:03:13 +0800

    try CFST

--- trainer_ft.py
+++ trainer_ft.py
@@ -90,7 +90,7 @@ class Trainer:
         self.oracle_flag = args.oracle_flag
         self.add_dim = self.num_tasks
 
-        args.schedule = [5]
+        args.schedule = [20]
 
         # Prepare the self.learner (model)
         self.learner_config = {'num_classes': num_classes,
