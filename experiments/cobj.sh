9b925986c126334b47a031aa55816d5039ae0522
pomelo <liaowd@mail.sustech.edu.cn>
2024-10-04 21:24:42 +0800

    run slot attn on imagenet-pretrained vit

--- experiments/cobj.sh
+++ experiments/cobj.sh
@@ -47,22 +47,27 @@ python -u run.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwri
 done
 done
 
-#lrs=(1e-4)
-#devices=(5)
-#for run_id in 0; do
-#lr=${lrs[${run_id}]}
-#device=${devices[${run_id}]}
-#LOGNAME=1-slot_prompt-minmax-k10-nt5-ptemp1-p30-l40-lr${lr}
+
+#lrs=(1e-4); temps=(5 11 31)
+#devices=(0 1 2); i=-1
+#for lr_run_id in 0; do
+#for temp_run_id in 0 1 2; do
+#((i++))
+#lr=${lrs[${lr_run_id}]}
+#temp=${temps[${temp_run_id}]}
+#device=${devices[${i}]}
+#LOGNAME=1-slot_prompt-minmax-k10-nt5-ptemp${temp}-p30-l40-lr${lr}
 #docker run -d --rm --runtime=nvidia --gpus device=${device} \
 #  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
 #  -v ~/.cache:/workspace/.cache \
 #  --shm-size 8G liaoweiduo/hide:2.0 \
 #python -u run.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
 #    --learner_type slotmo --learner_name SLOTPrompt \
-#    --prompt_param 30 40 10 5 1.0 1.0 0.0 0.0 0.1 1.2 \
+#    --prompt_param 30 40 10 5 1.0 ${temp} 0.0 0.0 0.1 1.2 \
 #    --slot_pre_learn_model 1-slot_attn-pos-k10-nt5-temp1-recon_noLN-slot_lr1e-4 \
 #    --lr ${lr} ${lr} \
 #    --log_dir ${OUTDIR}/${LOGNAME}
+#done
 #done
 ##    --t0_model_from slot-k10-p30-ccl0-l2weight0.05 \
 
