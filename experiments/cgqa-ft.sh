ec7764fdaf44b38c55129c6ea5ccd157437b71a9
pomelo <liaowd@mail.sustech.edu.cn>
2024-10-25 14:47:27 +0800

    cfst

--- experiments/cgqa-ft.sh
+++ experiments/cgqa-ft.sh
@@ -20,22 +20,22 @@ mkdir -p $OUTDIR
 
 
 # SLOT-Prompt
-for mode in sys pro sub non noc
-do
-  # do not use -d to avoid running in parallel
-  docker run --rm --runtime=nvidia --gpus device=2 \
-    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-    -v ~/.cache:/workspace/.cache \
-    --shm-size 8G liaoweiduo/hide:2.0 \
-  python -u run_ft.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-    --learner_type slotmo --learner_name SLOTPrompt \
-    --prompt_param 30 40 10 5 1.0 10 0.0 0.0 0.1 1.2 80 0.5 0.5 1 \
-    --log_dir ${OUTDIR}/5-slot_prompt-k10-nt5-ln-discrete_selec-cossim10-sol1-p30-l40-lr2e-4 \
-    --slot_pre_learn_model 4-slot_attn-pos-k10-nt5-recon_noLN-mk0.5-crosssim80-slot_vsI0.5-slot_lr1e-4 \
-    --lr 0.001 \
-    --mode ${mode}
-  date
-done
+#for mode in sys pro sub non noc
+#do
+#  # do not use -d to avoid running in parallel
+#  docker run --rm --runtime=nvidia --gpus device=2 \
+#    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+#    -v ~/.cache:/workspace/.cache \
+#    --shm-size 8G liaoweiduo/hide:2.0 \
+#  python -u run_ft.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#    --learner_type slotmo --learner_name SLOTPrompt \
+#    --prompt_param 30 40 10 5 1.0 10 0.0 0.0 0.1 1.2 80 0.5 0.5 1 \
+#    --log_dir ${OUTDIR}/5-slot_prompt-k10-nt5-ln-discrete_selec-cossim10-sol1-p30-l40-lr2e-4 \
+#    --slot_pre_learn_model 4-slot_attn-pos-k10-nt5-recon_noLN-mk0.5-crosssim80-slot_vsI0.5-slot_lr1e-4 \
+#    --lr 0.001 \
+#    --mode ${mode}
+#  date
+#done
 
 
 # CODA-P
@@ -76,12 +76,12 @@ done
 #    arg 1 = e-prompt pool size (# tasks)
 #    arg 2 = e-prompt pool length
 #    arg 3 = -1 -> shallow, 1 -> deep
-#for mode in sys pro sub non noc
-#do
-#  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#      --learner_type prompt --learner_name L2P \
-#      --prompt_param 10 10 -1 \
-#      --log_dir ${OUTDIR}/l2p++-imagenet-p10-l10 \
-#      --mode ${mode}
-#  date
-#done
+for mode in sys pro sub non noc
+do
+  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+      --learner_type prompt --learner_name L2P \
+      --prompt_param 10 10 -1 \
+      --log_dir ${OUTDIR}/l2p++-imagenet-p10-l10 \
+      --mode ${mode}
+  date
+done
