5e97884411cd85acfea3c2c9fe67c118a13dcea8
pomelo <liaowd@mail.sustech.edu.cn>
2024-11-08 23:15:36 +0800

    PCA LOSS use current CL classifier

--- learners/slotmo.py
+++ learners/slotmo.py
@@ -816,7 +816,7 @@ class SLOTPrompt(Prompt):
                 k_expand_logits, k_expand_features = self.model(
                     k_expand_inputs, q=k_expand_prompts,
                     pen=True, train=True,
-                    forward_last=False,         # only get features
+                    forward_last=True,         # False for only get features if use Aux classifier
                     debug_mode=self.debug_mode)
                 # features: [n_samples, 768]
                 # last = model.prompt.prompt_concept_alignment_classifier        # [c100, h128]
