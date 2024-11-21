753a38ae030ebca82c6bae526a04480f8754affb
pomelo <liaowd@mail.sustech.edu.cn>
2024-11-21 19:40:17 +0800

    CFST on vit pretrain

--- trainer_ft.py
+++ trainer_ft.py
@@ -92,7 +92,7 @@ class Trainer:
         self.oracle_flag = args.oracle_flag
         self.add_dim = self.num_tasks
 
-        args.schedule = [20]
+        args.schedule = [50]
 
         # Prepare the self.learner (model)
         self.learner_config = {'num_classes': num_classes,
