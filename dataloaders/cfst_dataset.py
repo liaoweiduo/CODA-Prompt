4fe5263408f985d563ef5990d9bad038e24530be
pomelo <liaowd@mail.sustech.edu.cn>
2024-12-01 00:07:57 +0800

    CODA diff lr

--- dataloaders/cfst_dataset.py
+++ dataloaders/cfst_dataset.py
@@ -255,7 +255,7 @@ class CGQA(CFSTDataset):
             self.benchmark = cgqa.continual_training_benchmark(
                 10, image_size=(224, 224), return_task_id=False,
                 seed=self.seed,
-                train_transform=cgqa._build_default_transform(is_train=True),
+                train_transform=cgqa._build_default_transform(image_size=(224, 224), is_train=True),
                 eval_transform=cgqa.build_transform_for_vit(is_train=False),
                 dataset_root=os.path.join(self.root, 'CFST'),
                 memory_size=0,
@@ -283,7 +283,7 @@ class COBJ(CFSTDataset):
             self.benchmark = cobj.continual_training_benchmark(
                 3, image_size=(224, 224), return_task_id=False,
                 seed=self.seed,
-                train_transform=cobj._build_default_transform(is_train=True),
+                train_transform=cobj._build_default_transform(image_size=(224, 224), is_train=True),
                 eval_transform=cobj.build_transform_for_vit(is_train=False),
                 dataset_root=os.path.join(self.root, 'CFST'),
                 memory_size=0,
