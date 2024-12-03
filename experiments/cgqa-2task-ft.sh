fa5bbf12fa2c3db160778c6327b6d701de0ac4bc
pomelo <liaowd@mail.sustech.edu.cn>
2024-12-03 15:24:27 +0800

    CODA diff lr

--- experiments/cgqa-2task-ft.sh
+++ experiments/cgqa-2task-ft.sh
@@ -52,8 +52,8 @@ do
     --shm-size 8G liaoweiduo/hide:2.0 \
   python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
       --learner_type prompt --learner_name CODAPrompt \
-      --prompt_param 100 40 0.0 \
-      --log_dir ${OUTDIR}/coda-imagenet-l40 \
+      --prompt_param 1 40 0.0 \
+      --log_dir ${OUTDIR}/MT-1p-l40 \
       --mode ${mode}
   date
 done
@@ -64,19 +64,19 @@ done
 #    arg 1 = e-prompt pool size (# tasks)
 #    arg 2 = e-prompt pool length
 #    arg 3 = g-prompt pool length
-for mode in sys pro sub non noc
-do
-  docker run --rm --runtime=nvidia --gpus device=6 \
-    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-    -v ~/.cache:/workspace/.cache \
-    --shm-size 8G liaoweiduo/hide:2.0 \
-  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-      --learner_type prompt --learner_name DualPrompt \
-      --prompt_param 10 40 10 \
-      --log_dir ${OUTDIR}/dual-prompt-imagenet-e40-g10 \
-      --mode ${mode}
-  date
-done
+#for mode in sys pro sub non noc
+#do
+#  docker run --rm --runtime=nvidia --gpus device=6 \
+#    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+#    -v ~/.cache:/workspace/.cache \
+#    --shm-size 8G liaoweiduo/hide:2.0 \
+#  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#      --learner_type prompt --learner_name DualPrompt \
+#      --prompt_param 10 40 10 \
+#      --log_dir ${OUTDIR}/dual-prompt-imagenet-e40-g10 \
+#      --mode ${mode}
+#  date
+#done
 
 # L2P++
 #
@@ -84,19 +84,19 @@ done
 #    arg 1 = e-prompt pool size (# tasks)
 #    arg 2 = e-prompt pool length
 #    arg 3 = -1 -> shallow, 1 -> deep
-for mode in sys pro sub non noc
-do
-  docker run --rm --runtime=nvidia --gpus device=6 \
-    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-    -v ~/.cache:/workspace/.cache \
-    --shm-size 8G liaoweiduo/hide:2.0 \
-  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-      --learner_type prompt --learner_name L2P \
-      --prompt_param 10 40 -1 \
-      --log_dir ${OUTDIR}/l2p++-imagenet-p10-l40 \
-      --mode ${mode}
-  date
-done
+#for mode in sys pro sub non noc
+#do
+#  docker run --rm --runtime=nvidia --gpus device=6 \
+#    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+#    -v ~/.cache:/workspace/.cache \
+#    --shm-size 8G liaoweiduo/hide:2.0 \
+#  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#      --learner_type prompt --learner_name L2P \
+#      --prompt_param 10 40 -1 \
+#      --log_dir ${OUTDIR}/l2p++-imagenet-p10-l40 \
+#      --mode ${mode}
+#  date
+#done
 
 # vit-pretrain
 #
