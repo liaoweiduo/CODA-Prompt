639c826f074c626214aa70556578381305e99b55
pomelo <liaowd@mail.sustech.edu.cn>
2024-12-05 11:52:19 +0800

    pmo-concept-1p-l40

--- experiments/cgqa-2task.sh
+++ experiments/cgqa-2task.sh
@@ -101,21 +101,21 @@ mkdir -p $OUTDIR
 #    arg 2 = prompt length
 #    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
 #    --oracle_flag --upper_bound_flag \
-LOGNAME=pmo-concept_w.9_.1-1p-l8-test0
-docker run -d --rm --runtime=nvidia --gpus device=1 \
-  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-  -v ~/.cache:/workspace/.cache \
-  --shm-size 8G liaoweiduo/hide:2.0 \
+LOGNAME=pmo-concept_w.9_.1-1p-l8
+#docker run -d --rm --runtime=nvidia --gpus device=1 \
+#  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+#  -v ~/.cache:/workspace/.cache \
+#  --shm-size 8G liaoweiduo/hide:2.0 \
 python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
     --learner_type pmo --learner_name PMOPrompt \
     --prompt_param 21 8 0.0 \
     --lr 0.001 \
     --concept_weight \
     --eval_class_wise \
-    --target_concept_id 0 \
     --oracle_flag --upper_bound_flag \
     --log_dir ${OUTDIR}/${LOGNAME}
 date
+#    --target_concept_id 0 \
 
 # CODA-P
 #
