ad403816b1e30c644325b45b43313d8dbcf1d5b6
pomelo <liaowd@mail.sustech.edu.cn>
2024-12-07 19:43:30 +0800

    pmo-concept-1p-l40

--- experiments/cgqa-2task-ft.sh
+++ experiments/cgqa-2task-ft.sh
@@ -20,22 +20,22 @@ mkdir -p $OUTDIR
 
 
 # SLOT-Prompt
-#for mode in sys pro sub non noc
-#do
-# # do not use -d to avoid running in parallel
-#  docker run --rm --runtime=nvidia --gpus device=6 \
-#    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-#    -v ~/.cache:/workspace/.cache \
-#    --shm-size 8G liaoweiduo/hide:2.0 \
-#  python -u run_ft.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type slotmo --learner_name SLOTPrompt \
-#    --prompt_param 100 40 10 5 1.0 10 0.0 0.0 0.0 0.0 80 0.0 0.0 0.0 0.0 \
-#    --log_dir ${OUTDIR}/2-slot_prompt-img_repr-sMT-p100-l40-k10-nt5-ln-wA-sigmoid-onehotl1-cossim10-l1_sol1-dilate1_contrast_cos_pcac0-lr1e-3 \
-#    --slot_pre_learn_model MT-slot_attn-pos-k10-nt5-recon_noLN-intra0.01-crosssim10-slot_vsI0.5-slot_lr1e-4 \
-#    --lr 0.001 \
-#    --mode ${mode}
-#  date
-#done
+for mode in sys pro sub non noc
+do
+ # do not use -d to avoid running in parallel
+  docker run --rm --runtime=nvidia --gpus device=1 \
+    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+    -v ~/.cache:/workspace/.cache \
+    --shm-size 8G liaoweiduo/hide:2.0 \
+  python -u run_ft.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type slotmo --learner_name SLOTPrompt \
+    --prompt_param 100 8 10 5 1.0 1.0 -1 0.0 0.0 80 0.0 0.0 0.0 0.0 \
+    --log_dir ${OUTDIR}/3-slot_prompt-sMT-p100-l8-k10-nt5-avgslot-lr1e-3 \
+    --slot_pre_learn_model MT-slot_attn-pos-k10-nt5-recon_noLN-intra0.01-crosssim10-slot_vsI0.5-slot_lr1e-4 \
+    --lr 0.001 \
+    --mode ${mode}
+  date
+done
 
 
 # CODA-P
@@ -44,19 +44,19 @@ mkdir -p $OUTDIR
 #    arg 1 = prompt component pool size
 #    arg 2 = prompt length
 #    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
-for mode in sys pro sub non noc
-do
-  docker run --rm --runtime=nvidia --gpus device=0 \
-    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-    -v ~/.cache:/workspace/.cache \
-    --shm-size 8G liaoweiduo/hide:2.0 \
-  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-      --learner_type prompt --learner_name CODAPrompt \
-      --prompt_param 100 8 0.0 \
-      --log_dir ${OUTDIR}/code-l8 \
-      --mode ${mode}
-  date
-done
+#for mode in sys pro sub non noc
+#do
+#  docker run --rm --runtime=nvidia --gpus device=0 \
+#    -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+#    -v ~/.cache:/workspace/.cache \
+#    --shm-size 8G liaoweiduo/hide:2.0 \
+#  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#      --learner_type prompt --learner_name CODAPrompt \
+#      --prompt_param 100 8 0.0 \
+#      --log_dir ${OUTDIR}/code-l8 \
+#      --mode ${mode}
+#  date
+#done
 
 # DualPrompt
 #
