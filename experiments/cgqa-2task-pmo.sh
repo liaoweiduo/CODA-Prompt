e2314256d541160312b7362df891d669f4e12b2e
pomelo <liaowd@mail.sustech.edu.cn>
2024-12-15 20:32:18 +0800

    pmo-concept-1p-l40

--- experiments/cgqa-2task-pmo.sh
+++ experiments/cgqa-2task-pmo.sh
@@ -27,33 +27,33 @@ mkdir -p $OUTDIR
 #    arg 2 = prompt length
 #    arg 3 = ortho penalty loss weight - with updated code, now can be 0!
 #    --oracle_flag --upper_bound_flag \
-LOGNAME=pmo-1p-task0-l8
-#docker run -d --rm --runtime=nvidia --gpus device=1 \
-#  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
-#  -v ~/.cache:/workspace/.cache \
-#  --shm-size 8G liaoweiduo/hide:2.0 \
-python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-    --learner_type pmo --learner_name PMOPrompt \
-    --prompt_param 1 8 0.0 \
-    --lr 0.001 \
-    --max_task 1 \
-    --target_concept_id 0 \
-    --eval_class_wise \
-    --log_dir ${OUTDIR}/${LOGNAME}
-date
+#LOGNAME=pmo-1p-task0-l8
+##docker run -d --rm --runtime=nvidia --gpus device=1 \
+##  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
+##  -v ~/.cache:/workspace/.cache \
+##  --shm-size 8G liaoweiduo/hide:2.0 \
+#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#    --learner_type pmo --learner_name PMOPrompt \
+#    --prompt_param 1 8 0.0 \
+#    --lr 0.001 \
+#    --max_task 1 \
 #    --target_concept_id 0 \
+#    --eval_class_wise \
+#    --log_dir ${OUTDIR}/${LOGNAME}
+#date
+##    --target_concept_id 0 \
 
 LOGNAME=pmo-1p-l8
-python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-    --learner_type pmo --learner_name PMOPrompt \
-    --prompt_param 1 8 0.0 \
-    --lr 0.001 \
-    --concept_weight \
-    --prompt_pre_learn_mode pmo-1p-task0-l8 \
-    --target_concept_id 0 \
-    --eval_class_wise \
-    --log_dir ${OUTDIR}/${LOGNAME}
-date
+#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#    --learner_type pmo --learner_name PMOPrompt \
+#    --prompt_param 1 8 0.0 \
+#    --lr 0.001 \
+#    --concept_weight \
+#    --prompt_pre_learn_mode pmo-1p-task0-l8 \
+#    --target_concept_id 0 \
+#    --eval_class_wise \
+#    --log_dir ${OUTDIR}/${LOGNAME}
+#date
 
 ## learn selection
 #LOGNAME=pmo-selection-concept_w.9_.1-1p-l8
@@ -67,18 +67,18 @@ date
 #    --log_dir ${OUTDIR}/${LOGNAME}
 #date
 
-## cfst
-#for mode in sys pro sub non noc
-#do
-# # do not use -d to avoid running in parallel
-#  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type pmo --learner_name PMOPrompt \
-#    --prompt_param 21 8 0.0 \
-#    --log_dir ${OUTDIR}/${LOGNAME} \
-#    --lr 0.001 \
-#    --mode ${mode}
-#  date
-#done
+# cfst
+for mode in sys pro sub non noc
+do
+ # do not use -d to avoid running in parallel
+  python -u run_ft.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type pmo --learner_name PMOPrompt \
+    --prompt_param 21 8 0.0 \
+    --log_dir ${OUTDIR}/${LOGNAME} \
+    --lr 0.001 \
+    --mode ${mode}
+  date
+done
 
 
 #LOGNAME=pmo-concept_w.9_.1-1p-l8-test0
