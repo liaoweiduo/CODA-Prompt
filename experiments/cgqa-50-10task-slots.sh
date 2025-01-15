3dfa494338b3084808e4aa9044199b8916220b0f
pomelo <liaowd@mail.sustech.edu.cn>
2025-01-15 17:04:27 +0800

    pmo-concept-1p-l40

--- experiments/cgqa-50-10task-slots.sh
+++ experiments/cgqa-50-10task-slots.sh
@@ -34,13 +34,12 @@ lr=1e-3
 slot_lr1=5e-4
 slot_lr2=1e-4
 intra_consistency_reg_coeff=$1
-#for intra_consistency_reg_coeff in 0.01 0.1 0.5; do
 intra_consistency_reg_mode=learn+l2
 
 slot_ortho_reg_coeff=0.5
 slot_ortho_reg_temp=0.1
 
-#slot_logit_similar_reg_coeff=0.01
+#for slot_logit_similar_reg_coeff in 0 0.01 0.1 0.5; do
 #slot_logit_similar_reg_temp=0.01
 #slot_logit_similar_reg_slot_temp=0.1
 
