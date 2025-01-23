f7a975e8d944162c69ce3c152af838d52b7031da
pomelo <liaowd@mail.sustech.edu.cn>
2025-01-23 16:07:50 +0800

    Update cgqa-50-10task-slots.sh

--- experiments/cgqa-50-10task-slots.sh
+++ experiments/cgqa-50-10task-slots.sh
@@ -92,7 +92,7 @@ intra_consistency_reg_coeff=0.5
 intra_consistency_reg_mode=cross+cos+kl
 
 slot_ortho_reg_mode=cos+ce
-slot_ortho_reg_coeff=1
+slot_ortho_reg_coeff=0.5
 
 SLOT_LOGNAME=29-slot_attn-icr${intra_consistency_reg_coeff}_m${intra_consistency_reg_mode}-sor${slot_ortho_reg_coeff}_m${slot_ortho_reg_mode}-slr${slot_lr1}_${slot_lr2}-pos-k10-nt5-recon_noLN
 #python -u run.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
