89862a4e72c2d064d41c1d26990833871102fe81
pomelo <liaowd@mail.sustech.edu.cn>
2025-01-24 18:55:28 +0800

    Update cgqa-50-10task-slots.sh

--- experiments/cgqa-50-10task-slots.sh
+++ experiments/cgqa-50-10task-slots.sh
@@ -89,7 +89,7 @@ slot_lr1=1e-4
 slot_lr2=1e-4
 
 intra_consistency_reg_coeff=$1   # learn 0.1， cross 0.5
-intra_consistency_reg_mode=learn+cos+kl
+intra_consistency_reg_mode=map+cos+kl
 
 slot_ortho_reg_mode=cos+ce
 slot_ortho_reg_coeff=0.5
@@ -102,7 +102,7 @@ SLOT_LOGNAME=34-slot_attn-e100-icr${intra_consistency_reg_coeff}_m${intra_consis
 python -u run.py --config $CONFIG_SLOT --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
     --learner_type slotmo --learner_name SLOTPrompt \
     --prompt_param 100 8 \
-    --batch_size 256 \
+    --batch_size 512 \
     --s2p_mode ${s2p_mode} \
     --s2p_temp ${s2p_temp} \
     --only_learn_slot \
