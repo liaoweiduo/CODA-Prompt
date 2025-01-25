233b2402cd87481dfb98123416635393f9994697
pomelo <liaowd@mail.sustech.edu.cn>
2025-01-25 14:23:19 +0800

    1

--- models/slot_attention.py
+++ models/slot_attention.py
@@ -417,7 +417,7 @@ class Slot2Prompt(nn.Module):
                 if 'avg' in self.cond_mode:
                     # use average slots as inputs to select prompt
                     slots_ = torch.einsum('bh,kh->bkh', avg_slots, A)      # attended slots
-                elif self.cond_mode in ['sig', 'soft', 'cos']:
+                elif self.cond_mode in ['sig', 'soft', 'cos', 'hard']:
                     slots_ = torch.einsum('bh,kh->bkh', w_slots, A)      # attended slots
                 else:
                     raise Exception(f'Un-implemented {self.cond_mode}.')
