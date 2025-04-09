2fc2c6c09abf55b193bc9df67fef63de7c91f4a4
pomelo <liaowd@mail.sustech.edu.cn>
2025-04-09 10:14:35 +0800

    1

--- experiments/cgqa-10task-baselines.sh
+++ experiments/cgqa-10task-baselines.sh
@@ -58,23 +58,23 @@ LOGNAME=coda
 #   --compositional_testing \
 #   --log_dir ${OUTDIR}/coda-p-f2s_linear
 
-# directly use concept to reg logits
-for concept_similar_reg_coeff in $1 $2 $3; do
-#concept_similar_reg_coeff=0.1
-concept_similar_reg_mode=dot+kl
-concept_similar_reg_temp=0.01    # 5 for cos
-python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-   --learner_type prompt --learner_name CODAPrompt \
-   --prompt_param 100 8 0.0 0 \
-   --lr 0.001 \
-   --concept_weight \
-   --concept_similar_reg_coeff ${concept_similar_reg_coeff} \
-   --concept_similar_reg_mode ${concept_similar_reg_mode} \
-   --concept_similar_reg_temp ${concept_similar_reg_temp} \
-   --compositional_testing \
-   --max_task 2 \
-   --log_dir ${OUTDIR}/coda-p-concept-${concept_similar_reg_coeff}
-done
+## directly use concept to reg logits
+#for concept_similar_reg_coeff in $1 $2 $3; do
+##concept_similar_reg_coeff=0.1
+#concept_similar_reg_mode=dot+kl
+#concept_similar_reg_temp=0.01    # 5 for cos
+#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+#   --learner_type prompt --learner_name CODAPrompt \
+#   --prompt_param 100 8 0.0 0 \
+#   --lr 0.001 \
+#   --concept_weight \
+#   --concept_similar_reg_coeff ${concept_similar_reg_coeff} \
+#   --concept_similar_reg_mode ${concept_similar_reg_mode} \
+#   --concept_similar_reg_temp ${concept_similar_reg_temp} \
+#   --compositional_testing \
+#   --max_task 2 \
+#   --log_dir ${OUTDIR}/coda-p-concept-${concept_similar_reg_coeff}
+#done
 
 # DualPrompt
 #
@@ -86,13 +86,13 @@ done
 #  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
 #  -v ~/.cache:/workspace/.cache \
 #  --shm-size 8G liaoweiduo/hide:2.0 \
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name DualPrompt \
-#    --prompt_param 10 20 6 \
-#    --lr 0.001 \
-#    --do_not_eval_during_training \
-#    --compositional_testing \
-#    --log_dir ${OUTDIR}/dual-prompt
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name DualPrompt \
+    --prompt_param 10 20 6 \
+    --lr 0.001 \
+    --do_not_eval_during_training \
+    --compositional_testing \
+    --log_dir ${OUTDIR}/dual-prompt
 
 
 # L2P++
@@ -105,13 +105,13 @@ done
 #  -v ~/CODA-Prompt:/workspace -v /mnt/datasets/datasets:/workspace/data -v ~/checkpoints:/checkpoints \
 #  -v ~/.cache:/workspace/.cache \
 #  --shm-size 8G liaoweiduo/hide:2.0 \
-#python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
-#    --learner_type prompt --learner_name L2P \
-#    --prompt_param 30 20 -1 \
-#    --lr 0.001 \
-#    --do_not_eval_during_training \
-#    --compositional_testing \
-#    --log_dir ${OUTDIR}/l2p++
+python -u run.py --config $CONFIG --gpuid $GPUID --repeat $REPEAT --overwrite $OVERWRITE \
+    --learner_type prompt --learner_name L2P \
+    --prompt_param 30 20 -1 \
+    --lr 0.001 \
+    --do_not_eval_during_training \
+    --compositional_testing \
+    --log_dir ${OUTDIR}/l2p++
 
 
 # vit-pretrain
